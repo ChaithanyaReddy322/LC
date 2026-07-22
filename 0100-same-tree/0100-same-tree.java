929982de7fbaa0bfb4e44f817a8cbe953a67ad20
Remata Chaithanya Reddy <chaitanyareddyremata@gmail.com>
2026-07-22 23:20:52 +0530

    Time: 0 ms (100%), Space: 42.8 MB (52.67%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,9 +15,10 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-       if(p==null || q==null) {
-        return (p==q);
-       }
-       return (p.val==q.val) && isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+        boolean check = true;
+        if(p==null && q==null) return true;
+        if((p==null && q!=null)|| (p!=null && q==null)|| (p.val!=q.val)) return false;
+        check=isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+        return check;
     }
 }
